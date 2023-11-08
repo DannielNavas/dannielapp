1eaf46f69c7810de0e7ec190e36ce51b1be0078a
DannielNavas <dannielnavas@gmail.com>
2023-11-08 06:23:25 -0500

    ✨ Add React Navigation dependencies and wrap app in NavigationContainer

--- App.js
+++ App.js
@@ -1,32 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
-import DefautlProps from './src/components/DefaultProps';
-import LoginForm from './src/components/LoginForm';
-import Saludar from './src/components/Saludar';
-import ValidateProps from './src/components/ValidateProps';
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import DefautlProps from "./src/components/DefaultProps";
+import LoginForm from "./src/components/LoginForm";
+import Saludar from "./src/components/Saludar";
+import ValidateProps from "./src/components/ValidateProps";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <LoginForm />
-      <Saludar name="danniel navas" />
-      <Saludar name="leslye navarro" />
-      <Text>Default</Text>
-      <DefautlProps />
-      <DefautlProps firstname="Leslye" lastname="Navarro" />
-      <Text>Validate</Text>
-      <ValidateProps firstname="Leslye" lastname="Navarro" />
-      {/* <ValidateProps  /> */}
-
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <LoginForm />
+        <Saludar name="danniel navas" />
+        <Saludar name="leslye navarro" />
+        <Text>Default</Text>
+        <DefautlProps />
+        <DefautlProps firstname="Leslye" lastname="Navarro" />
+        <Text>Validate</Text>
+        <ValidateProps firstname="Leslye" lastname="Navarro" />
+        {/* <ValidateProps  /> */}
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
